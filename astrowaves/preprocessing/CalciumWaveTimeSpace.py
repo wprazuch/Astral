6ccd359a4c4ef86e084f54b104fe84ecf4df7fa6
wojciechprazuch <wojciechprazuch3@gmail.com>
2020-02-03 22:11:39 +0100

    Updated

--- astrowaves/preprocessing/CalciumWaveTimeSpace.py
+++ astrowaves/preprocessing/CalciumWaveTimeSpace.py
@@ -5,7 +5,9 @@ import cv2 as cv
 import nrrd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 
 class CalciumWaveTimeSpace():
@@ -15,17 +17,21 @@ class CalciumWaveTimeSpace():
 
     def create_time_space(self, path_to_image_sequence):
         slices = [slic for slic in os.listdir(path_to_image_sequence) if slic.endswith('.tif') and not slic.startswith('.')]
-        print(slices)
-        I = plt.imread(''.join([path_to_image_sequence, slices[1199]]))
-        image_matrix = np.ndarray(shape=I.shape+(len(slices),), dtype='uint8')
-        print(slices[-5:])
+        no_slices = len(slices)
+        logging.info("Detected {} image slices".format(no_slices))
+        I = plt.imread(''.join([path_to_image_sequence, slices[no_slices-1]]))
+        img_shape = I.shape
+        logging.info("Detected image shape of {} pixels".format(img_shape))
+        image_matrix = np.ndarray(shape=img_shape+(no_slices,), dtype='uint8')
+        logging.info("Merging images into 3d representation...")
         for i, slic in enumerate(tqdm(slices)):
             image_matrix[:, : , i] = plt.imread(''.join([path_to_image_sequence, slices[i]]))
-
+        logging.info("Done.")
         return image_matrix
         
 
     def create_calcium_waves_map(self, image_matrix):
+        logging.info("Extracting calcium waves from 3d representation - might take a while...")
         background = np.mean(image_matrix, axis=2).astype('int16')
         background = background.reshape(background.shape+(1,))
         waves = np.ndarray(shape=image_matrix.shape, dtype='int16')
@@ -35,5 +41,7 @@ class CalciumWaveTimeSpace():
         ptp = np.ptp(waves).astype('int16')
         waves_norm = 255*((waves-minn)/ptp)
         waves_norm = waves_norm.astype('uint8')
+
+        del waves, background
 
         return waves_norm
