4c7e7338641d13bb1c4850c1e6906413ef6eba5a
WojPrauch <wojciechprazuch3@gmail.com>
2020-06-08 20:32:08 +0200

    update

--- astrowaves/gather/CalciumWaveDetector.py
+++ astrowaves/gather/CalciumWaveDetector.py
@@ -4,6 +4,10 @@ import skimage
 from skimage import measure
 from joblib import Parallel, delayed
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 class CalciumWaveDetector():
 
@@ -14,33 +18,52 @@ class CalciumWaveDetector():
         return np.argwhere(array == label)
 
     def run2(self, waves):
-        waves_labelled = measure.label(waves, connectivity=3).astype('uint16')
-        uniq, counts = np.unique(waves_labelled, return_counts=True)
+        waves[waves == 255] = 1
+        waves = waves.astype('bool')
+        slices = [i for i in range(waves.shape[2]) if not np.any(waves[:, :, i])]
+        slic = slices[int(len(slices)/2)]
+        first, second = waves[:, :, :slic], waves[:, :, slic:]
+        logging.info('Labelling object pixels...')
+        first = measure.label(first, connectivity=3).astype('uint16')
+        second = measure.label(second, connectivity=3).astype('uint16')
+        second = second + first.max() + 1
+        second[second == first.max() + 1] = 0
+        waves = np.concatenate([first, second], axis=2)
+        logging.info('Filtering small objects under 30 pixels big...')
+        uniq, counts = np.unique(waves, return_counts=True)
         labels = uniq[1:]
         counts = counts[1:]
         label_counts = list(zip(labels, counts))
         count_filtered = list(filter(lambda x: x[1] > 30, label_counts))
         labels, counts = zip(*count_filtered)
+        logging.info('Grouping object pixels...')
         object_cords = Parallel(n_jobs=3, verbose=10)(delayed(self._indices_label)
-                                                      (waves_labelled, label) for label in labels)
+                                                      (waves, label) for label in labels)
         return object_cords
 
 
-if __name__ == '__main__':
-
-    debug_path = '/app/data/output_data'
-
+def debug():
+    debug_path = r'C:\Users\Wojtek\Documents\Doktorat\Astral\data\output_data'
     waves = np.load(os.path.join(debug_path, "waves_morph.npy"))
-
     detector = CalciumWaveDetector()
-
-    #wave_inds = np.argwhere(waves == 255)
-
     waves_inds = detector.run2(waves)
-
     import pickle
 
     with open(os.path.join(debug_path, 'waves_inds.pck'), 'wb') as file:
         pickle.dump(waves_inds, file)
 
-    #seg = region_grow(waves, wave_inds[2])
+
+def __main__():
+    debug_path = '/app/data/output_data'
+
+    waves = np.load(os.path.join(debug_path, "waves_morph.npy"))
+    detector = CalciumWaveDetector()
+    waves_inds = detector.run2(waves)
+    import pickle
+    with open(os.path.join(debug_path, 'waves_inds.pck'), 'wb') as file:
+        pickle.dump(waves_inds, file)
+
+
+if __name__ == '__main__':
+    __main__()
+    # debug()
--- astrowaves/preprocessing/CalciumWavesExtractor.py
+++ astrowaves/preprocessing/CalciumWavesExtractor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import logging
+from tqdm import tqdm
 
 
 class CalciumWavesExtractor():
@@ -14,15 +15,15 @@ class CalciumWavesExtractor():
         logging.info("Extracting calcium waves from 3d representation - might take a while...")
         background = np.mean(image_matrix, axis=2).astype('int16')
         background = background.reshape(background.shape+(1,))
-        image_matrix = (image_matrix-background)
+        image_matrix = (image_matrix - background)
         image_matrix[image_matrix < 0] = 0
-        waves_norm = np.ndarray(shape=image_matrix.shape, dtype='int16')
         minn = np.min(image_matrix).astype('int16')
         ptp = np.ptp(image_matrix).astype('int16')
-        waves_norm = 255*((image_matrix-minn)/ptp)
-        waves_norm = waves_norm.astype('uint8')
+        for i in tqdm(range(image_matrix.shape[2])):
+            image_matrix[:, :, i] = 255*((image_matrix[:, :, i]-minn)/ptp)
+            image_matrix = image_matrix.astype('uint8')
 
-        return waves_norm
+        return image_matrix
 
 
 def __main__():
